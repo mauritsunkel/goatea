1fcbbe79f79f9fe21e0d554f6bec898ac732b515
Maurits Unkel <mauritsunkel@gmail.com>
2025-01-10 15:57:55 +0100

    Refactored server code to match read_validate_genelist()

--- R/server.R
+++ R/server.R
@@ -14,29 +14,33 @@ server <- function(input, output, session) {
   # Reactive value to store the file path
   filepaths_DE <- shiny::reactiveVal(NULL)
   go <- shiny::reactiveValues()
+  genelists <- shiny::reactiveValues()
   
   # Observe the button click and open file browser
   shiny::observeEvent(input$load_DE, {
     filepaths <- utils::choose.files()
-    genelists <- read_genelist(
-      filepaths, 
-      remove_NA_ids = input$cbi_remove_NA_ids, 
-      remove_duplicated = input$cbi_remove_duplicated,
-      remove_Rik_genes = input$cbi_remove_Rik_genes,
-      remove_Gm_genes = input$cbi_remove_Gm_genes,
-      keep_maxN_genes = input$cbi_keep_maxN_genes)
-    
-    if(is.character(genelists)) {
-      shinyjs::runjs("$('#filepaths_DE').css('color', 'red');")
-      filepaths_DE(paste(genelists))
-      shinyjs::hide("run_goat_tab")
-      shinyjs::hide("go_to_goat")
-    } else {
-      shinyjs::runjs("$('#filepaths_DE').css('color', 'green');")
-      filepaths_DE(paste(basename(filepaths), sapply(genelists, nrow)))
-      shinyjs::show("go_to_goat")
-      shinyjs::showElement(id = "run_goat_tab_out")
-      go$data_genelist <- genelists
+    for (file in filepaths) {
+      genelist <- read_validate_genelist(
+        file = file, 
+        remove_NA_ids = input$cbi_remove_NA_ids, 
+        remove_duplicated = input$cbi_remove_duplicated,
+        remove_Rik_genes = input$cbi_remove_Rik_genes,
+        remove_Gm_genes = input$cbi_remove_Gm_genes,
+        keep_maxN_genes = input$cbi_keep_maxN_genes)
+      
+      if(is.character(genelist)) {
+        shinyjs::runjs("$('#filepaths_DE').css('color', 'red');")
+        filepaths_DE(paste(genelist))
+        shinyjs::hide("run_goat_tab")
+        shinyjs::hide("go_to_goat")
+      } else {
+        genelists[[file]] <- genelist
+        shinyjs::runjs("$('#filepaths_DE').css('color', 'green');")
+        filepaths_DE(paste(basename(filepaths), nrow(genelist)))
+        shinyjs::show("go_to_goat")
+        shinyjs::showElement(id = "run_goat_tab_out")
+        go$data_genelist <- genelist
+      }
     }
   })
   
