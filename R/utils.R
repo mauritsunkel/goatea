297279bce58a65ce1063060ce95a2c3db4fc58c4
Maurits Unkel <mauritsunkel@gmail.com>
2025-01-10 13:35:53 +0100

    Added check_dependency()

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,39 @@ file_extension <- function (x) {
   pos <- regexpr("\\.([[:alnum:]]+)$", x)
   ifelse(pos > -1L, substring(x, pos + 1L), "")
 }
+
+#' throw error if R package is unavailable
+#'
+#' @param pkg R package name
+#' @param msg function name / reference for user
+#' @noRd
+check_dependency = function(pkg, msg) {
+  if(!requireNamespace(pkg, quietly = TRUE)) {
+    stop(paste0(
+      "An optional dependency for ",
+      msg,
+      " is not installed; R package '",
+      pkg,
+      "' is not available. For convenience, you may use the following command to install all dependencies (including optional) for the 'goat' R package; pak::pkg_install('ftwkoopmans/goat', dependencies = TRUE)"
+    ), call. = FALSE)
+  }
+}
+
+#' Fix Excel formatting genes to dates back to gene names.
+#'
+#' @param gene_names gene names (where some are dates) in character vector
+#'
+#' @return gene names in character vector
+date2gene <- function(gene_names) {
+  ## if .csv was saved in Excel, some gene names e.g. SEP2 become dates
+  # get index of these genes
+  excel_genes_ind <- grepl(x = gene_names, pattern = "^[A-Z][a-z]{2}/[0-9]{2}$")
+  # get these genes
+  excel_genes <- gene_names[excel_genes_ind]
+  # convert back to original names from date names
+  gene_names[excel_genes_ind] <- sapply(X = excel_genes, USE.NAMES = FALSE, FUN = function(X) {
+    ss <- unlist(strsplit(X, split = '/'))
+    paste0(toupper(ss[1]), as.integer(ss[2]))
+  })
+  return(unlist(gene_names))
+}
