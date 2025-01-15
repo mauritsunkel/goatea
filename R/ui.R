c8467da7f8cd5ead74a52014365e105aea1eb51b
Maurits Unkel <mauritsunkel@gmail.com>
2025-01-15 10:11:43 +0100

    Updated UI, testing radioButtons

--- R/ui.R
+++ R/ui.R
@@ -16,13 +16,25 @@ ui <- function() {
       tabItems(
         tabItem(tabName = "load_data",
                 fluidRow(
-                  column(width = 2, actionButton("load_DE", "Load DE")),
-                  column(width = 2, checkboxInput("cbi_remove_NA_ids", "Remove NA IDs", value = TRUE)),
-                  column(width = 2, checkboxInput("cbi_remove_duplicated", "Remove duplicated", value = TRUE)),
-                  column(width = 2, checkboxInput("cbi_remove_Rik_genes", "Remove Rik genes", value = TRUE)),
-                  column(width = 2, checkboxInput("cbi_remove_Gm_genes", "Remove Gm genes", value = TRUE)),
-                  column(width = 2, checkboxInput("cbi_keep_maxN_genes", "Keep max N genes", value = TRUE)),
+                  tags$div(title="Select human or mouse for loading Gene Ontology Bioconductor genesets", # tooltip
+                    column(width = 2, radioButtons("rb_human_mouse", "Organism:"))),
+                ),
+                fluidRow(
+                  tags$div(title="Load DE data: .csv, .xlsx, .tsv", # tooltip
+                    column(width = 2, actionButton("load_DE", "Load DE"))),
+                  tags$div(title="Remove genes with non-available IDs", # tooltip
+                  column(width = 2, checkboxInput("cbi_remove_NA_ids", "Remove NA IDs", value = TRUE))),
+                  tags$div(title="Remove duplicated genes", # tooltip
+                    column(width = 2, checkboxInput("cbi_remove_duplicated", "Remove duplicated", value = TRUE))),
+                  tags$div(title="Remove Riken non-canonical mouse genes", # tooltip
+                    column(width = 2, checkboxInput("cbi_remove_Rik_genes", "Remove Rik genes", value = TRUE))),
+                  tags$div(title="Remove Gm non-canonical mouse genes", # tooltip
+                  column(width = 2, checkboxInput("cbi_remove_Gm_genes", "Remove Gm genes", value = TRUE))),
+                  tags$div(title=paste0("Filter down to max allowed n genes: ", max(goat::goat_nulldistributions$N)), # tooltip
+                  column(width = 2, checkboxInput("cbi_keep_maxN_genes", "Keep max N genes", value = TRUE))),
                   column(width = 12, verbatimTextOutput("vto_filepaths_DE", placeholder = TRUE)),
+
+                  # TODO just run GOAT, no need to load more files for user, always fast and up-to-date
                   column(width = 2, actionButton("load_GOAT", "Load GOAT results (optional)")),
                   column(width = 12, verbatimTextOutput("filepaths_GOAT", placeholder = TRUE)),
                   box(
