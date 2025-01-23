4c6b42c44cea3b9f86ebe27257c074dc06725429
Maurits Unkel <mauritsunkel@gmail.com>
2025-01-23 10:34:51 +0100

    Added wrap_loader() and wrap_hovertip() for Shiny UI elements, also added get_gene_annotation()

--- R/utils.R
+++ R/utils.R
@@ -20,3 +20,50 @@ check_dependency = function(pkg, msg) {
     ), call. = FALSE)
   }
 }
+
+
+#' Wrap Shiny UI element with a loading spinner contained in html div tags
+#'
+#' @param id string: id of loader, used with show/hide in server side
+#' @param ui_element wrapped Shiny UI element
+#'
+#' @export
+#'
+wrap_loader <- function(id, ui_element) {
+  div(ui_element,
+      div(
+        id = id, 
+        style = "display:none;", 
+        icon("spinner", class = "fa-spin", style = "font-size: 20px; color: #008000;")
+      ))}
+
+#' Wrap Shiny UI element with a hoverable tooltip contained in html div tags
+#'
+#' @param text 
+#' @param ui_element 
+#'
+#' @export
+wrap_hovertip <- function(text, ui_element) {
+  tags$div(title = text, ui_element)
+}
+
+#' Get gene annotation for mouse
+#'
+#' Can extend functino to get gene annotations for other organisms within annotables package.
+#'
+#' @param gene_symbols character vector of ensembl genes 109 symbols
+#' @param organism default "Hs" (Homo Sapiens), otherwise: "Mm" (Mus Musculus)
+#'
+#' @export
+get_gene_annotation <- function(gene_symbols, organism = "Hs") {
+  if (organism == "Hs") {
+    annodata <- annotables::grch38
+  } else if (organism == "Mm") {
+    annodata <- annotables::grcm38
+  }
+  return(plyr::mapvalues(
+    x = gene_symbols,
+    from = annodata$symbol,
+    to = annodata$description,
+    warn_missing = FALSE))
+}
