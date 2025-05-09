d0c3cf4641f8fd2df7e7d9aaa8d1ef4b86745ea7
Maurits Unkel <mauritsunkel@gmail.com>
2025-05-09 18:08:31 +0200

    Updated docs, set F to FALSE, removed old commented out function

--- R/plot_genelists_overlap.R
+++ R/plot_genelists_overlap.R
@@ -12,9 +12,12 @@
 #' 
 #' @importFrom upsetjs upsetjs generateIntersections generateDistinctIntersections generateUnions chartTheme interactiveChart
 #'
+#' @description
+#' UpSetJS examples: https://upset.js.org/integrations/r/articles/combinationModes.html#distinct-intersection-mode
+#' 
 #' @examples
-#' # https://upset.js.org/integrations/r/articles/combinationModes.html#distinct-intersection-mode
-plot_genelists_overlap_upsetjs <- function(genelists, mode = 'distinct', interactive = FALSE, main.color = 'white', highlight.color = 'green') {
+#' plot_genelists_overlap_upsetjs(list(A = goatea::example_genelist, B = goatea::example_genelist))
+plot_genelists_overlap_upsetjs <- function(genelists, mode = 'distinct', interactive = FALSE, main.color = 'black', highlight.color = 'green') {
   col <- c("#ff00ff", "olivedrab", "#7f0000", "#4b0082", "#ff0000",
            "orange", "#ffff00", "#00ff00", "#00fa9a", "#00ffff", "#0000ff",
            "darkslategray", "#6495ed", "#ffe4b5", "#ff69b4")
@@ -48,7 +51,10 @@ plot_genelists_overlap_upsetjs <- function(genelists, mode = 'distinct', interac
 #' @importFrom ggVennDiagram Venn process_data venn_regionedge venn_setedge venn_setlabel venn_regionlabel
 #' @importFrom ggplot2 ggplot aes geom_polygon geom_path geom_text geom_label coord_equal scale_fill_manual theme_void scale_x_continuous expansion
 #' 
-#' @return plot to render
+#' @returns plot to render
+#' 
+#' @examples
+#' plot_genelists_overlap_venn(list(A = goatea::example_genelist, B = goatea::example_genelist))
 plot_genelists_overlap_venn <- function(genelists) {
   
   genelists_gene_overlaps <- lapply(genelists, function(df) df$gene[df$signif == TRUE])
@@ -72,8 +78,8 @@ plot_genelists_overlap_venn <- function(genelists) {
   # DEVNOTE: for coloring edges: 1) where geom_polygon(aes(fill = id)) --> geom_polygon(aes(fill = I(col)), where col = metadata column
   # DEVNOTE: for coloring edges: 2) ggplot() + scale_fill_manual(values = col), where col = metadata column (of venn_region_edge)
   p <- ggplot2::ggplot() +
-    ggplot2::geom_polygon(ggplot2::aes(.data$X, .data$Y, fill = .data$id, group = .data$id), data = ggVennDiagram::venn_regionedge(d), show.legend = F) +
-    ggplot2::geom_path(ggplot2::aes(.data$X, .data$Y, color = .data$id, group = .data$id), data = ggVennDiagram::venn_setedge(d), show.legend = F) +
+    ggplot2::geom_polygon(ggplot2::aes(.data$X, .data$Y, fill = .data$id, group = .data$id), data = ggVennDiagram::venn_regionedge(d), show.legend = FALSE) +
+    ggplot2::geom_path(ggplot2::aes(.data$X, .data$Y, color = .data$id, group = .data$id), data = ggVennDiagram::venn_setedge(d), show.legend = FALSE) +
     ggplot2::geom_text(ggplot2::aes(.data$X, .data$Y, label = .data$name), fontface = "bold", data = ggVennDiagram::venn_setlabel(d)) +
     ggplot2::geom_label(ggplot2::aes(.data$X, .data$Y, label = .data$count), data = ggVennDiagram::venn_regionlabel(d)) +
     ggplot2::coord_equal() +
@@ -81,75 +87,3 @@ plot_genelists_overlap_venn <- function(genelists) {
     ggplot2::theme_void() + ggplot2::scale_x_continuous(expand = ggplot2::expansion(mult = .2))
   return(p)
 }
-
-## DEPRECATED: UpsetR plot version
-# Visualize genelists gene overlap in UpSet plot
-# 
-# @param genelists UI value/list of tibbles/dataframes
-# @param grayscale_colors default: UI valaue/FALSE, else TRUE, for colorful or grayscale degrees in UpSet plot 
-# @param empty_intersections default: UI/value/TRUE, if FALSE, shouldn't print intersects with 0 overlaps, error in UpSetR package
-# 
-# @export 
-# 
-# @importFrom UpSetR intersects upset
-# 
-# @return plot to render
-# plot_genelists_overlap_upsetR <- function(genelists, grayscale_colors = TRUE, empty_intersections = TRUE) {
-#   genelists_gene_overlaps <- lapply(genelists, function(df) df$gene[df$signif == TRUE])
-#   
-#   col <- c("#ff00ff", "olivedrab", "#7f0000", "#4b0082", "#ff0000",
-#            "orange", "#ffff00", "#00ff00", "#00fa9a", "#00ffff", "#0000ff",
-#            "darkslategray", "#6495ed", "#ffe4b5", "#ff69b4")
-#   
-#   set <- names(genelists_gene_overlaps)
-#   
-#   queries = list()
-#   combinations <- do.call(c, lapply(seq_along(set), combn, x = set, simplify = FALSE))
-#   for (i in seq_along(combinations)) {
-#     combi <- combinations[[i]]
-#     
-#     if ( ! grayscale_colors) {
-#       color <- col[i]
-#     } else {
-#       ## color by grayscale based on intersection degree
-#       color <- switch(
-#         length(combi),
-#         "grey0",
-#         "grey20",
-#         "grey40",
-#         "grey60",
-#         "grey80",
-#         "grey90"
-#       )
-#     }
-#     
-#     name <- switch(
-#       length(combi),
-#       "Degree 1",
-#       "Degree 2",
-#       "Degree 3",
-#       "Degree 4",
-#       "Degree 5",
-#       "Degree 6"
-#     )
-#     queries[[length(queries)+1]] <- list(
-#       query = UpSetR::intersects,
-#       params = combi,
-#       active = TRUE,
-#       color = color,
-#       query.name = name
-#     )
-#   }
-#   
-#   upd <- UpSetR::fromList(genelists_gene_overlaps)
-#   if (sum(upd[, 1]) == 0) upd <- upd[, c(2,1)] # fix error if first col all zeroes
-#   
-#   ## if intersection is not shown it is empty (no overlap)
-#   p <- UpSetR::upset(data = upd, sets = set, nintersects = NA, order.by = "freq", shade.color = "grey100",
-#                      sets.bar.color = "black", main.bar.color = "black", matrix.color = "black",
-#                      queries = queries, empty.intersections = empty_intersections, query.legend = "none",
-#                      mb.ratio = c(0.7, 0.3), text.scale = 1,
-#                      sets.x.label = "Significant genes",
-#                      set_size.show = TRUE, set_size.scale_max = 1000)
-#   return(p)
-# }
