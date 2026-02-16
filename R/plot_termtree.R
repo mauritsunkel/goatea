198742a145b130acf732d9aa72378c4587d45d2b
Maurits Unkel <mauritsunkel@gmail.com>
2026-02-16 17:46:41 +0100

    Fixed example

--- R/plot_termtree.R
+++ R/plot_termtree.R
@@ -17,7 +17,7 @@
 #' 
 #' @examples
 #' plot_termtree(get(load(system.file("extdata", "example_genelist.rda", package = "goatea"))))
-plot_termtree <- function(genelist, map_organism = 9606, effectsize_threshold = 1, Nterms = NA, Nwords = 5, Nclusters = 1) {
+plot_termtree <- function(genelist, map_organism = 9606, effectsize_threshold = 1, Nterms = NA, Nwords = 5, Nclusters = 3) {
   if ( ! requireNamespace("enrichplot")) stop("Need 'enrichplot' package installed via: BiocManager::install('enrichplot')")
   if (length(genelist) == 0) stop("List passed to plot termtree has no genes/proteins")
   if (is.na(Nterms) || Nterms > length(genelist)) Nterms <- length(genelist)
