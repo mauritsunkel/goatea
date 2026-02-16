131c87213ca0596e79a6c75e56d3eff7196a3663
Maurits Unkel <mauritsunkel@gmail.com>
2026-02-16 17:33:34 +0100

    Fixed example

--- R/plot_termtree.R
+++ R/plot_termtree.R
@@ -15,7 +15,7 @@
 #' @importFrom enrichplot pairwise_termsim treeplot
 #' 
 #' @examples
-#' plot_termtree(genelist <- get(load(system.file("extdata", "example_genelist.rda", package = "goatea"))))
+#' plot_termtree(get(load(system.file("extdata", "example_genelist.rda", package = "goatea"))))
 plot_termtree <- function(genelist, map_organism = 9606, effectsize_threshold = 1, Nterms = NA, Nwords = 5, Nclusters = 1) {
   if ( ! requireNamespace("enrichplot")) stop("Need 'enrichplot' package installed via: BiocManager::install('enrichplot')")
   if (length(genelist) == 0) stop("List passed to plot termtree has no genes/proteins")
